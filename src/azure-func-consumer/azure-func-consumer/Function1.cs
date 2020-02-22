0b1df3f8cd0e40991c34b6e30c5fec6458ea8ef2
NPSPOT\Pavel.Daineka <pavel.daineka@nordpoolgroup.com>
2020-02-22 16:00:24 +0200

    azure function rabbit init

--- src/azure-func-consumer/azure-func-consumer/Function1.cs
+++ src/azure-func-consumer/azure-func-consumer/Function1.cs
@@ -12,21 +12,13 @@ namespace azure_func_consumer
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task Run([RabbitMQTrigger("Keda.Contracts_All_AF", ConnectionStringSetting = "RabbitMQConnection")] string inputMessage,
+
+            ILogger logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var msq = inputMessage;
+            await Task.CompletedTask;
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
